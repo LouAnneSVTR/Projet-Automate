e81f8e066b0e61efe63304ef3db98c1cde90c853
LouAnneSVTR <louanne.sauvetre@gmail.com>
2020-12-21 15:52:27 +0100

    test main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include "set.hpp"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
+    univ_nantes::set<int> s = {1, 2, 3, 4};
+
+    univ_nantes::set<int> sBis;
+
+    sBis = s;
+
+
+
     return 0;
 }
